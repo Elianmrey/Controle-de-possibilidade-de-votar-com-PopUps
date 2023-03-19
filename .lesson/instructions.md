f3f60ec3eed16e91f95a0bc44214a633d174059b
ELIAN M. REYES <62479495+Elianmrey@users.noreply.github.com>
2023-03-19 20:46:43 +0000

    Initial commit

---
+++ .lesson/instructions.md
@@ -0,0 +1,11 @@
+<p align="center">
+    <img src="https://www.infnet.edu.br/infnet/wp-content/themes/infnet.homepage//assets/img/LogoInfnetRodape.png"/>
+</p>
+
+# Teste de Performance 4
+
+## Exercício 05
+
+Faça uma função que pede ao usuário, utilizando popup, a sua idade e informa ao mesmo, também por popup, se o mesmo pode ou não votar.
+
+Execute a função após criá-la!
---
+++ script.js
@@ -0,0 +1,61 @@
+
+const botao = document.querySelector(".iniciar")
+const mostrarPopup = document.querySelector(".popup-mostrar");
+const popUpDoisFechar = document.querySelector(".popupdois");
+botao.addEventListener("click", () => {
+  popUpDoisFechar.style.display = "none";
+  mostrarPopup.style.display = "block";
+});
+
+
+/* Função que controla o fluxo de informação do usuario e devolve outro popup mostrando o controle de idade*/
+
+// Mostrando segundo popup e verificando Maioridae
+
+const botaoAceitar = document.querySelector(".aceitar");
+const popUpResposta = document.querySelector(".popupdois");
+const fecharPopUp = document.querySelector(".popup-mostrar");
+botaoAceitar.addEventListener("click", () => {
+  //verificando Idade
+  const infoIdade = Number(document.getElementById("caixa-texto").value);
+  if (infoIdade >= 18) {
+    const podeVotar = 'Você pode votar';
+    document.querySelector("#texto").innerHTML = `${podeVotar}`;
+  } else {
+    const naoPodeVotar = 'Você não pode votar';
+    document.querySelector("#texto").innerHTML = `${naoPodeVotar}`;
+  }
+  // Fechando Popup que solicita Idade e Mostrando Popup Resposta
+
+  popUpResposta.style.display = "block";
+  fecharPopUp.style.display = "none"
+});
+
+// Fechar com botão X de Popup-Mostrar
+
+const popUpFechar = document.querySelector(".close-popup")
+popUpFechar.addEventListener("click", () => {
+  fecharPopUp.style.display = "none";
+  console.log("Fechou com botão X, a validção foi cancelada");
+});
+
+
+// fechar com botão fechar do segundo popup
+
+const botaoClose = document.querySelector(".fechapop");
+const popUpClose = document.querySelector(".popupdois");
+botaoClose.addEventListener("click", () => {
+  popUpClose.style.display = "none";
+  console.log("Fechou com botão fechar");
+});
+
+//Fechar com botão X do PopupDois 
+
+const fecharComX = document.querySelector(".fechar")
+fecharComX.addEventListener("click", () => {
+  popUpClose.style.display = "none";
+  console.log("Fechou com botão X");
+});
+
+
+
